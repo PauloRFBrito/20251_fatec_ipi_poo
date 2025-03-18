4648dca88ec8d927837ba3f65fb7c1ae4af2b43d
Paulo Brito <pricardof27@gmail.com>
2025-03-18 18:43:53 -0300

    feat: feat: implementacao da estrutura menu.py

---
+++ Documents/poo/calc_py/menu.py
@@ -0,0 +1,39 @@
+import calculadora
+
+def menu():
+    while True:
+        print("\n---- MENU ----")
+        print("1. Somar")
+        print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+
+        opcao = input("Selecione uma opção: \n")
+
+        if opcao == "0":
+            print("Encerando a aplicação!")
+            break
+
+        if opcao in ["1", "2", "3", "4"]:
+            try:
+                a = float(input("Digite o primeiro valor: \n"))
+                b = float(input("Digite o segundo valor: \n"))
+
+                if opcao == "1":
+                    print("Resultado: \n", calculadora.somar(a, b))
+                elif opcao == "2":
+                    print("Resultado: \n", calculadora.subtrair(a, b))
+                elif opcao == "3":
+                    print("Resultado: \n", calculadora.multiplicar(a, b))
+                elif opcao == "4":
+                    if b != 0:
+                        print("Resultado: \n", calculadora.dividir(a, b))
+                    else:
+                        print("Divisão por zero não permitida!\n")
+            except ValueError:
+                print("Erro, digite valores válidos!\n")
+        else:
+            print("Opção inválida, tente novamente!\n")
+
+menu()
