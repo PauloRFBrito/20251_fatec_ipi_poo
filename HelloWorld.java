d002e8497cc3b555cf957642fabe65cbe03ea257
Paulo Brito <pricardof27@gmail.com>
2025-03-20 13:23:00 -0300

    learn: hello world em java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//Metódo simples
+/*
+public class HelloWorld{
+    public static void main(String [] args){
+        System.out.println("Hello, World!");
+    }
+}
+*/
+
+//Impressão com dialogo utilizando a classe JOptionPane e o método showMessageDialog 
+
+import javax.swing.JOptionPane;
+public class HelloWorld{
+    public static void main(String [] args){
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
